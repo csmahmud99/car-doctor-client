2d87505815bb49f85451e12aedd4173dcbb19fe0
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-18 00:06:05 +0600

    Login Form created with HERO by DaisyUI --- Done

--- src/Pages/Login/Login/Login.jsx
+++ src/Pages/Login/Login/Login.jsx
@@ -1,34 +1,42 @@
-
+import loginImage from "../../../assets/images/login/login.svg";
 
 const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+    };
+
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <div className="hero min-h-screen bg-base-200 pt-20 pb-20">
+                <div className="hero-content flex-col lg:flex-row gap-20 max-w-6xl mx-auto">
+                    <div className="w-1/2">
+                        <img src={loginImage} alt="login-page-image" />
                     </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card flex-shrink-0 w-1/2 max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
-                            </div>
+                            <h1 className="text-3xl font-bold text-center text-red-600 mb-5">Login</h1>
+                            <form onSubmit={handleLogin}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="text" placeholder="Your email" className="input input-bordered" />
+                                </div>
+
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Confirm Password</span>
+                                    </label>
+                                    <input type="text" placeholder="Your password" className="input input-bordered" />
+                                    <label className="label">
+                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    </label>
+                                </div>
+
+                                <div className="form-control mt-6">
+                                    <input type="submit" value="Sign In" className="btn btn-outline bg-red-600 text-white" />
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
