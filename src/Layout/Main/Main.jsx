a880da95432267ba91c89ee82806ef665633c18b
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-17 10:38:45 +0600

    Primary Route Set-up, Basic Components & Outlet Set-up, Header Navigation &  Footer from DaisyUI, Full Site Center Aligned from Tailwind --- Done

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Footer from '../../Pages/Shared/Footer/Footer';
+import NavigationBar from '../../Pages/Shared/NavigationBar/NavigationBar';
+
+const Main = () => {
+    return (
+        <div>
+            <NavigationBar />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
+
+export default Main;
