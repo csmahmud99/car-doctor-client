c2d248c9d0f010cfa3d2c2425ae25d74bc482aa5
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-19 02:30:55 +0600

    Booked Single Item Delete --- Done

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -9,6 +9,24 @@ const Bookings = () => {
 
     const [bookings, setBookings] = useState([]);
 
+    const handleDelete = _id => {
+        const proceed = confirm("Are you sure you want to delete this?");
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${_id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert("Deleted Successfully");
+                        const remaining = bookings.filter(booking => booking._id !== _id);
+                        setBookings(remaining);
+                    }
+                });
+        }
+    };
+
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
@@ -16,8 +34,8 @@ const Bookings = () => {
     }, []);
 
     return (
-        <div className="max-w-6xl mx-auto">
-            <h2>Your Bookings: {bookings.length}</h2>
+        <div className="max-w-6xl mx-auto pt-20 pb-20">
+            <h2 className="text-center text-4xl text-red-600 mb-5 font-bold">No. of Your Booked Items: {bookings.length}</h2>
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
                     {/* head */}
@@ -25,20 +43,21 @@ const Bookings = () => {
                         <tr>
                             <th>
                                 <label>
-                                    <input type="checkbox" className="checkbox" />
+                                    Cancel Order
                                 </label>
                             </th>
                             <th>Service Name & Customer Name</th>
                             <th>Price</th>
                             <th>Date of Getting the Service</th>
-                            <th></th>
+                            <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             bookings.map(booking => <BookingRow
                                 key={booking._id}
-                                booking={booking}
+                                booking={booking} 
+                                handleDelete={handleDelete}
                             />)
                         }
                     </tbody>
